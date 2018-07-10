b4835306ab6b3f12b43b692a5678253b34c0a151
Alexandr <alexandr.kizilow@gmail.com>
2018-07-10 06:22:01 +0300

    add ssl connect to database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize( process.env.DATABASE_URL);
+const sequelize = new Sequelize( process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: true,
+  operatorAliases: false,
+  ssl: true,
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 const db = {};
 
