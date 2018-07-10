5dc8ef85a8141ba8aea83a10bbc8b5a2b0b15aeb
Alexandr <alexandr.kizilow@gmail.com>
2018-07-10 14:07:33 +0300

    add initialize function

--- index.js
+++ index.js
@@ -7,39 +7,50 @@ require('dotenv').config();
 
 const app = express();
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+function initializeHandlebars(app) {  
+  app.engine('handlebars', handlebars.engine);
+  app.set('view engine', 'handlebars');
+}
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cookieSession({
-  name: process.env.nameSession,
-  secret: process.env.secretSession,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    path: '/',
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000
-  }
-}));
-app.use(express.static('static'));
+function initializePassport(app) {
+  app.use(passport.initialize());
+  app.use(passport.session()); 
+}
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+function initializeSession(app) {
+  app.use(cookieSession({
+    name: process.env.nameSession,
+    secret: process.env.secretSession,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })); 
+}
+function initializeStatic () {
+  app.use(express.static('static'));
+}
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.clientID,
-  clientSecret: process.env.clientSecret,
-  callbackURL: process.env.callbackURL
-},
-function(accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}));
+function setStrategy() {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  passport.use(new GoogleStrategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: process.env.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }));    
+}
 
 app.get('/albums', (req, res, next) => {
   if (req.session.passport) {
