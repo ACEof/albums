0a798bab220af4290e738df5371ac3c547b79cb8
Alexandr <alexandr.kizilow@gmail.com>
2018-07-12 19:47:54 +0300

    delete comment

--- models/users.js
+++ models/users.js
@@ -5,7 +5,6 @@ const Users = db.define('users',
   {
     id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
     username: Sequelize.STRING(100),
-    //allowNull: false,
   });
 
 module.exports = Users;
