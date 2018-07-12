16f431ac350d5a42bac720f77fa9edda29cdfb98
Alexandr <alexandr.kizilow@gmail.com>
2018-07-12 16:58:03 +0300

    rename addres post

--- parser.js
+++ parser.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 function parser(app) {
   const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-  app.post('/register', urlencodedParser, (req, res) => {
+  app.post('/add', urlencodedParser, (req, res) => {
     if (!req.body) {
       return res.sendStatus(400);
     }
