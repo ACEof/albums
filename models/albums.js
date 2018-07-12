a9688d591be4f20ca6adc476403b80ca82185ec2
Alexandr <alexandr.kizilow@gmail.com>
2018-07-12 20:05:09 +0300

    add albums model and add album to database

---
+++ models/albums.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+const db = require('./dataBase');
+
+const Albums = db.define('albums', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+  albumTitle: Sequelize.STRING
+});
+
+module.exports = Albums;
--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const Album = require('./models/albums');
 
 function parser(app) {
   const urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -8,7 +9,13 @@ function parser(app) {
       return res.sendStatus(400);
     }
     res.render('albums', {albumTitle: req.body.albumTitle});
+    createOrFindAlbum(req.body.albumTitle);
   });
+}
+
+function createOrFindAlbum(title) {
+  Album
+    .findOrCreate({where: {albumTitle: title}});
 }
 
 module.exports = parser;
