e7fc694e024b656d43cd44cce8b5addd47a7ee84
Alexandr <alexandr.kizilow@gmail.com>
2018-07-18 20:19:15 +0300

    add display of albums created by the user

--- routers/albums.js
+++ routers/albums.js
@@ -2,18 +2,19 @@ const User = require('../models/users');
 const Album = require('../models/albums');
 
 module.exports = function albums (app) {
-  app.get('/albums', (req, res) => {
+  app.get('/albums', async (req, res) => {    
     if (req.session.passport) {
       User.findOrCreateUser(req.session.passport.user.displayName);
-      User.findUserId(req.session.passport.user.displayName)
-        .then((id) => {
-          Album.selectAlbumTitle(id[0].id)
-            .then((select) => {
-              console.log(select);
-            });
-        });
-      return res.render('albums', {name: req.session.passport.user.displayName});
-    }  
+      const userID = await User.findUserId(req.session.passport.user.displayName);
+      const albumTitle = await Album.selectAlbumTitle(userID[0].id);
+      const renderAlbumTitle = {};
+      
+      for(const i in albumTitle){
+        renderAlbumTitle[i] = albumTitle[i].albumtitle;
+      }
+      
+      return res.render('albums', {name: req.session.passport.user.displayName, title: renderAlbumTitle});
+    }
     res.redirect('/auth/google');
   });
 };
