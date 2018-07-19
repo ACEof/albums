513368c8ce91c2eb51966eb4478454a3e30b4be0
Alexandr <alexandr.kizilow@gmail.com>
2018-07-19 13:24:36 +0300

    add possibility to remove albums

--- models/albums.js
+++ models/albums.js
@@ -12,10 +12,17 @@ function createAlbum(title, id) {
     .create({albumtitle: title, user_id: id});
 }
 
+function deleteAlbum(title){
+  Albums
+    .destroy({where: {
+      albumtitle: title
+    }});
+}
+
 const selectAlbumTitle = async (userID) => {
   const albumTitile = await db.query('SELECT albumtitle from albums where user_id = ?', 
     {replacements: [userID], type: db.QueryTypes.SELECT})
   return albumTitile;  
 }
 
-module.exports = {Albums, createAlbum, selectAlbumTitle};
+module.exports = {Albums, createAlbum, selectAlbumTitle, deleteAlbum};
---
+++ routers/deleteAlbum.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const Albums = require('../models/albums');
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function deleteAlbum(app) {
+  app.post('/options', urlencodedParser, (req, res) => {
+    if (!req.body) {
+      return res.sendStatus(400);
+    }
+    
+    Albums.deleteAlbum(req.body.albumTitle);
+    res.redirect('/albums');
+  })
+}
