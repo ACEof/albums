bb2c98929314c61806ed71257dbd9cfe5c4221b4
Alexandr <alexandr.kizilow@gmail.com>
2018-07-23 21:33:23 +0300

    Revert "delete comments in migrations"
    This reverts commit 47847eaa03571607df2c177111743aefd5fac3ef.

--- migrations/20180716122450-rename-column-into-albums.js
+++ migrations/20180716122450-rename-column-into-albums.js
@@ -6,5 +6,12 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
   }
 };
