1900a55d7bbebc46f46c3887d26edc3a23437ba5
Alexandr <alexandr.kizilow@gmail.com>
2018-07-23 22:16:33 +0300

    add return in user.js

--- models/users.js
+++ models/users.js
@@ -10,6 +10,7 @@ const Users = db.define('users',
   const findUserId = async (userName) => {
     const select = await db.query('SELECT id from users where username = ?', 
        { replacements: [userName], type: db.QueryTypes.SELECT})
+    return select;
  }
 
  function findOrCreateUser(name){
