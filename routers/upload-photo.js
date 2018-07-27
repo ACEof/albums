8a40d3846dbc50c5ea454d97410bc5cb71e2aae5
Alexandr <alexandr.kizilow@gmail.com>
2018-07-27 12:48:09 +0300

    change gitignore

--- routers/upload-photo.js
+++ routers/upload-photo.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
+const fs = require('fs');
 
 module.exports = function uploadPhoto(app) {
   app.post('/upload-photo', upload.single('photo'), (req, res) => {
