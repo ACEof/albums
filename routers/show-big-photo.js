06fc9f57074111695ddea61c7d558cbbec1804e7
Alexandr <alexandr.kizilow@gmail.com>
2018-08-06 15:35:57 +0300

    add display big photo

---
+++ routers/show-big-photo.js
@@ -0,0 +1,6 @@
+module.exports = function showBigPhoto (app) {
+  app.get('/photo-*', (req, res) => {
+    let photoName = req.url.substr(7);
+    res.render('show-big-photo', {photo: photoName});
+  });
+};
